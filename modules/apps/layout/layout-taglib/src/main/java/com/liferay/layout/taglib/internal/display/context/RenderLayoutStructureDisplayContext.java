b2d94a06f05a8fbaafbfcd551bf096ad109ba341
Iván Zaera Avellón <ivan.zaera@liferay.com>
2020-10-07 10:58:13 +0200

    LPS-121610 Create a constant for cssVariable mapping type

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/display/context/RenderLayoutStructureDisplayContext.java
@@ -20,6 +20,7 @@ import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
 import com.liferay.frontend.token.definition.FrontendTokenDefinition;
 import com.liferay.frontend.token.definition.FrontendTokenDefinitionRegistry;
+import com.liferay.frontend.token.definition.FrontendTokenMapping;
 import com.liferay.info.constants.InfoDisplayWebKeys;
 import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.ClassPKInfoItemIdentifier;
@@ -719,7 +720,10 @@ public class RenderLayoutStructureDisplayContext {
 			return styleValue;
 		}
 
-		return "var(--" + styleValueJSONObject.getString("cssVariable") + ")";
+		String cssVariable = styleValueJSONObject.getString(
+			FrontendTokenMapping.TYPE_CSS_VARIABLE);
+
+		return "var(--" + cssVariable + ")";
 	}
 
 	private String _getBackgroundImage(JSONObject rowConfigJSONObject)
@@ -915,7 +919,7 @@ public class RenderLayoutStructureDisplayContext {
 
 						if (Objects.equals(
 								mappingJSONObject.getString("type"),
-								"cssVariable")) {
+								FrontendTokenMapping.TYPE_CSS_VARIABLE)) {
 
 							cssVariable = mappingJSONObject.getString("value");
 						}
@@ -939,7 +943,7 @@ public class RenderLayoutStructureDisplayContext {
 					_frontendTokensJSONObject.put(
 						name,
 						JSONUtil.put(
-							"cssVariable", cssVariable
+							FrontendTokenMapping.TYPE_CSS_VARIABLE, cssVariable
 						).put(
 							"value", value
 						));
