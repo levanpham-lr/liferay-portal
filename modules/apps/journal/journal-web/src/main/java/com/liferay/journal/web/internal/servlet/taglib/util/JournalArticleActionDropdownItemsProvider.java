ecb48287f161d6b60b63b0b8538f9212a395f8c4
Hugo Huijser <hugo.huijser@liferay.com>
2020-11-12 13:06:14 +0800

    Revert "LPS-115709 Standardize mvc.command.name - journal"
    This reverts commit 2ba32f018d39b3eaacd32ff6c0fed7cfb806cff7.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/util/JournalArticleActionDropdownItemsProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/util/JournalArticleActionDropdownItemsProvider.java
@@ -303,7 +303,7 @@ public class JournalArticleActionDropdownItemsProvider {
 				_liferayPortletResponse.createActionURL();
 
 			copyArticleURL.setParameter(
-				ActionRequest.ACTION_NAME, "/journal/copy_article");
+				ActionRequest.ACTION_NAME, "copyArticle");
 
 			copyArticleURL.setParameter("redirect", _getRedirect());
 			copyArticleURL.setParameter(
@@ -316,7 +316,7 @@ public class JournalArticleActionDropdownItemsProvider {
 				"autoArticleId", Boolean.TRUE.toString());
 
 			return dropdownItem -> {
-				dropdownItem.putData("action", "/journal/copy_article");
+				dropdownItem.putData("action", "copyArticle");
 				dropdownItem.putData(
 					"copyArticleURL", copyArticleURL.toString());
 				dropdownItem.setLabel(
