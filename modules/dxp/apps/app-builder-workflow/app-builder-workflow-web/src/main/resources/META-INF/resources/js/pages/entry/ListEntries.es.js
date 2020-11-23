d9acd67281c3c061089236bd93658310c64ee808
Nara Andrade <nara.andrade@liferay.com>
2020-11-23 11:49:33 -0300

    LPS-122489 Show edit action only when the user is assigned to the entry

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/entry/ListEntries.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/entry/ListEntries.es.js
@@ -42,7 +42,7 @@ import ReassignEntryModal from './ReassignEntryModal.es';
 const WORKFLOW_COLUMNS = [
 	{key: 'status', value: Liferay.Language.get('status')},
 	{key: 'taskNames', value: Liferay.Language.get('step')},
-	{key: 'assignees', value: Liferay.Language.get('assignee')},
+	{key: 'assignee', value: Liferay.Language.get('assignee')},
 ];
 
 export default function ListEntries({history}) {
@@ -193,10 +193,10 @@ export default function ListEntries({history}) {
 
 				WORKFLOW_COLUMNS.forEach(({key}) => {
 					switch (key) {
-						case 'assignees': {
+						case 'assignee': {
 							const {assignees = [], taskNames = []} = entry;
 
-							const {id, name = emptyValue, reviewer} =
+							const {id = -1, name = emptyValue, reviewer} =
 								assignees[0] || {};
 
 							if (id === -1) {
@@ -282,7 +282,9 @@ export default function ListEntries({history}) {
 			show: ({canReassign}) => canReassign,
 		},
 		...useEntriesActions({
-			update: ({completed}) => completed === false,
+			update: ({assignees, completed}) =>
+				completed === false &&
+				assignees?.[0]?.id === Number(themeDisplay.getUserId()),
 		}),
 	];
 
