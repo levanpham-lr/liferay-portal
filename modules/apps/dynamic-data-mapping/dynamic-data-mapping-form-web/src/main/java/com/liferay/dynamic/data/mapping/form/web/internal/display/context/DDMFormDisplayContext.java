a1e9f361aa81d461d08d4dabb7cbb261644bc6bd
Jose Abelenda <jose.abelenda@liferay.com>
2020-12-07 10:04:49 -0300

    LPS-124584 checking is remember me is marked

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -63,6 +63,7 @@ import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.CookieKeys;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -418,6 +419,18 @@ public class DDMFormDisplayContext {
 		}
 
 		return _hasViewPermission;
+	}
+
+	public boolean isAutoRememberMe() {
+		String autoRememberMe = CookieKeys.getCookie(
+			PortalUtil.getHttpServletRequest(_renderRequest),
+			CookieKeys.REMEMBER_ME);
+
+		if ((autoRememberMe != null) && autoRememberMe.equals("true")) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public boolean isAutosaveEnabled() throws PortalException {
