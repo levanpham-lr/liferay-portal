f806138aaf31129815f236b7ff8059b499271619
Brian Chan <brian.chan@liferay.com>
2020-12-12 10:21:33 -0800

    LPS-121139 auto SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
@@ -1868,28 +1868,6 @@ public class LayoutStagedModelDataHandler
 			addGuestPermissions);
 	}
 
-	private boolean _isLayoutOutdated(Layout layout, Layout layoutPrototype) {
-		if ((layout == null) || (layoutPrototype == null)) {
-			return true;
-		}
-
-		Date layoutModifiedDate = layout.getModifiedDate();
-
-		long lastMergeTime = GetterUtil.getLong(
-			layout.getTypeSettingsProperty(Sites.LAST_MERGE_TIME));
-
-		Date layoutPrototypeModifiedDate = layoutPrototype.getModifiedDate();
-
-		if ((layoutModifiedDate == null) ||
-			(layoutPrototypeModifiedDate == null) ||
-			(layoutPrototypeModifiedDate.getTime() > lastMergeTime)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
 	@Override
 	protected boolean isSkipImportReferenceStagedModels() {
 		return true;
@@ -2401,6 +2379,28 @@ public class LayoutStagedModelDataHandler
 		}
 
 		return layout.getLayoutPrototypeUuid();
+	}
+
+	private boolean _isLayoutOutdated(Layout layout, Layout layoutPrototype) {
+		if ((layout == null) || (layoutPrototype == null)) {
+			return true;
+		}
+
+		Date layoutModifiedDate = layout.getModifiedDate();
+
+		long lastMergeTime = GetterUtil.getLong(
+			layout.getTypeSettingsProperty(Sites.LAST_MERGE_TIME));
+
+		Date layoutPrototypeModifiedDate = layoutPrototype.getModifiedDate();
+
+		if ((layoutModifiedDate == null) ||
+			(layoutPrototypeModifiedDate == null) ||
+			(layoutPrototypeModifiedDate.getTime() > lastMergeTime)) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private Layout _updateCollectionLayoutTypeSettings(
