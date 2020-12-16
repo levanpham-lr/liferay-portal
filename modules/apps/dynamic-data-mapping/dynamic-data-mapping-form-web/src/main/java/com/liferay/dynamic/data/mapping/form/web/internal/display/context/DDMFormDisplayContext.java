e7ac5b11ac7b597a51c3b20d30efb4f22d4b0d76
Brian Chan <brian.chan@liferay.com>
2020-12-15 21:08:40 -0800

    LPS-124584 auto SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -421,18 +421,6 @@ public class DDMFormDisplayContext {
 		return _hasViewPermission;
 	}
 
-	public boolean isRememberMe() {
-		String rememberMe = CookieKeys.getCookie(
-			PortalUtil.getHttpServletRequest(_renderRequest),
-			CookieKeys.REMEMBER_ME);
-
-		if ((rememberMe != null) && rememberMe.equals("true")) {
-			return true;
-		}
-
-		return false;
-	}
-
 	public boolean isAutosaveEnabled() throws PortalException {
 		if (_autosaveEnabled != null) {
 			return _autosaveEnabled;
@@ -521,6 +509,18 @@ public class DDMFormDisplayContext {
 				themeDisplay.getPermissionChecker(), getFormInstanceId(),
 				ActionKeys.UPDATE)) {
 
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isRememberMe() {
+		String rememberMe = CookieKeys.getCookie(
+			PortalUtil.getHttpServletRequest(_renderRequest),
+			CookieKeys.REMEMBER_ME);
+
+		if ((rememberMe != null) && rememberMe.equals("true")) {
 			return true;
 		}
 
