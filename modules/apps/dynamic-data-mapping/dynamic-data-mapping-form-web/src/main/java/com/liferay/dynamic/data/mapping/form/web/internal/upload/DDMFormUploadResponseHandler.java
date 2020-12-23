8cf656b62d60fe73a15ad82497b4772e4f4af19b
Carolina <carolina.barbosa@liferay.com>
2020-12-23 13:20:22 -0300

    LPS-125132 Include generic error message when json object is empty

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadResponseHandler.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadResponseHandler.java
@@ -49,9 +49,7 @@ public class DDMFormUploadResponseHandler implements UploadResponseHandler {
 		JSONObject jsonObject = _defaultUploadResponseHandler.onFailure(
 			portletRequest, portalException);
 
-		JSONObject errorJSONObject = jsonObject.getJSONObject("error");
-
-		String errorMessage = errorJSONObject.getString("message");
+		String errorMessage = StringPool.BLANK;
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
@@ -77,14 +75,12 @@ public class DDMFormUploadResponseHandler implements UploadResponseHandler {
 		else if (portalException instanceof InvalidFileException) {
 			errorMessage = themeDisplay.translate("please-enter-a-valid-file");
 		}
+		else {
+			errorMessage = themeDisplay.translate(
+				"an-unexpected-error-occurred-while-uploading-your-file");
+		}
 
-		return jsonObject.put(
-			"error",
-			JSONUtil.put(
-				"errorType", errorJSONObject.getInt("errorType")
-			).put(
-				"message", errorMessage
-			));
+		return jsonObject.put("error", JSONUtil.put("message", errorMessage));
 	}
 
 	@Override
