d6c34a1772e931247fbd2bc825daada68b3f7db9
Brian Chan <brian.chan@liferay.com>
2020-12-25 10:49:53 -0800

    LPS-125070 try/finally

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/messaging/AssetEntriesCheckerHelper.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/messaging/AssetEntriesCheckerHelper.java
@@ -161,6 +161,8 @@ public class AssetEntriesCheckerHelper {
 
 		_notifySubscribers(subscriptions, portletPreferences, newAssetEntries);
 
+		NotifiedAssetEntryThreadLocal.setNotifiedAssetEntryIdsModified(true);
+
 		try {
 			portletPreferences.setValues(
 				"notifiedAssetEntryIds",
@@ -168,16 +170,14 @@ public class AssetEntriesCheckerHelper {
 					ListUtil.toString(
 						assetEntries, AssetEntry.ENTRY_ID_ACCESSOR)));
 
-			NotifiedAssetEntryThreadLocal.setNotifiedAssetEntryIdsModified(
-				true);
-
 			portletPreferences.store();
-
-			NotifiedAssetEntryThreadLocal.setNotifiedAssetEntryIdsModified(
-				false);
 		}
 		catch (IOException | PortletException exception) {
 			throw new PortalException(exception);
+		}
+		finally {
+			NotifiedAssetEntryThreadLocal.setNotifiedAssetEntryIdsModified(
+				false);
 		}
 	}
 
