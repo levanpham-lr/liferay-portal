c27774fc84006763c16d933497964c7a7ea8fd0b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-01-12 16:48:55 +0100

    LPS-125755 Don't break the layout copy if a portlet preferences doesn't exist

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
@@ -32,6 +32,8 @@ import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
 import com.liferay.portal.kernel.comment.CommentManager;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Image;
 import com.liferay.portal.kernel.model.Layout;
@@ -386,10 +388,20 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 		}
 
 		for (String portletId : targetPortletIds) {
-			_portletPreferencesLocalService.deletePortletPreferences(
-				PortletKeys.PREFS_OWNER_ID_DEFAULT,
-				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, targetLayout.getPlid(),
-				portletId);
+			try {
+				_portletPreferencesLocalService.deletePortletPreferences(
+					PortletKeys.PREFS_OWNER_ID_DEFAULT,
+					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, targetLayout.getPlid(),
+					portletId);
+			}
+			catch (Exception exception) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						"Unable to delete portlet preferences for portlet " +
+							portletId,
+						exception);
+				}
+			}
 		}
 	}
 
@@ -618,6 +630,9 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 
 		return layoutStructure.toJSONObject();
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutCopyHelperImpl.class);
 
 	private static final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
