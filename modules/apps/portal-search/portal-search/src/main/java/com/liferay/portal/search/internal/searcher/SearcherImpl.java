brandonyee333/test

LPS-125505 portal-search: Deprecate method returning classes from SearchRequest; use class names instead of classes.
https://issues.liferay.com/browse/LPS-125505

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/searcher/SearcherImpl.java
@@ -87,11 +87,13 @@ public class SearcherImpl implements Searcher {
 		SearchRequestImpl searchRequestImpl,
 		SearchResponseBuilder searchResponseBuilder) {
 
-		Class<?> singleIndexerClass = getSingleIndexerClass(searchRequestImpl);
+		String singleIndexerClassName = getSingleIndexerClassNames(
+			searchRequestImpl);
 
-		if (singleIndexerClass != null) {
+		if (singleIndexerClassName != null) {
 			doSingleIndexerSearch(
-				singleIndexerClass, searchRequestImpl, searchResponseBuilder);
+				singleIndexerClassName, searchRequestImpl,
+				searchResponseBuilder);
 		}
 		else {
 			doMultiIndexerSearch(searchRequestImpl, searchResponseBuilder);
@@ -157,10 +159,10 @@ public class SearcherImpl implements Searcher {
 	}
 
 	protected void doSingleIndexerSearch(
-		Class<?> clazz, SearchRequestImpl searchRequestImpl,
+		String singleIndexerClassName, SearchRequestImpl searchRequestImpl,
 		SearchResponseBuilder searchResponseBuilder) {
 
-		Indexer<?> indexer = indexerRegistry.getIndexer(clazz);
+		Indexer<?> indexer = indexerRegistry.getIndexer(singleIndexerClassName);
 
 		SearchContext searchContext = searchRequestImpl.getSearchContext();
 
@@ -201,14 +203,14 @@ public class SearcherImpl implements Searcher {
 			searchRequestContributor -> searchRequestContributor::contribute);
 	}
 
-	protected Class<?> getSingleIndexerClass(
+	protected String getSingleIndexerClassNames(
 		SearchRequestImpl searchRequestImpl) {
 
-		List<Class<?>> modelIndexerClasses =
-			searchRequestImpl.getModelIndexerClasses();
+		List<String> modelIndexerClassNames =
+			searchRequestImpl.getModelIndexerClassNames();
 
-		if (modelIndexerClasses.size() == 1) {
-			return modelIndexerClasses.get(0);
+		if (modelIndexerClassNames.size() == 1) {
+			return modelIndexerClassNames.get(0);
 		}
 
 		return null;

levanpham-lr/liferay-portal

modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/searcher/SearcherImpl.java
@@ -87,11 +87,13 @@ public class SearcherImpl implements Searcher {
 		SearchRequestImpl searchRequestImpl,
 		SearchResponseBuilder searchResponseBuilder) {
 
-		Class<?> singleIndexerClass = getSingleIndexerClass(searchRequestImpl);
+		String singleIndexerClassName = getSingleIndexerClassName(
+			searchRequestImpl);
 
-		if (singleIndexerClass != null) {
+		if (singleIndexerClassName != null) {
 			doSingleIndexerSearch(
-				singleIndexerClass, searchRequestImpl, searchResponseBuilder);
+				singleIndexerClassName, searchRequestImpl,
+				searchResponseBuilder);
 		}
 		else {
 			doMultiIndexerSearch(searchRequestImpl, searchResponseBuilder);
@@ -157,10 +159,10 @@ public class SearcherImpl implements Searcher {
 	}
 
 	protected void doSingleIndexerSearch(
-		Class<?> clazz, SearchRequestImpl searchRequestImpl,
+		String singleIndexerClassName, SearchRequestImpl searchRequestImpl,
 		SearchResponseBuilder searchResponseBuilder) {
 
-		Indexer<?> indexer = indexerRegistry.getIndexer(clazz);
+		Indexer<?> indexer = indexerRegistry.getIndexer(singleIndexerClassName);
 
 		SearchContext searchContext = searchRequestImpl.getSearchContext();
 
@@ -201,14 +203,14 @@ public class SearcherImpl implements Searcher {
 			searchRequestContributor -> searchRequestContributor::contribute);
 	}
 
-	protected Class<?> getSingleIndexerClass(
+	protected String getSingleIndexerClassName(
 		SearchRequestImpl searchRequestImpl) {
 
-		List<Class<?>> modelIndexerClasses =
-			searchRequestImpl.getModelIndexerClasses();
+		List<String> modelIndexerClassNames =
+			searchRequestImpl.getModelIndexerClassNames();
 
-		if (modelIndexerClasses.size() == 1) {
-			return modelIndexerClasses.get(0);
+		if (modelIndexerClassNames.size() == 1) {
+			return modelIndexerClassNames.get(0);
 		}
 
 		return null;