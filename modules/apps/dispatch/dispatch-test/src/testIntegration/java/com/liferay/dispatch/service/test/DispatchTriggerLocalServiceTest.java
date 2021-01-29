90238a7fcc061993afada1d414510cbc44731dcb
Matija Petanjek <matija.petanjek@liferay.com>
2021-01-29 12:30:27 +0100

    COMMERCE-5396 dispatch-test - user CronExpressionUtil

--- modules/apps/dispatch/dispatch-test/src/testIntegration/java/com/liferay/dispatch/service/test/DispatchTriggerLocalServiceTest.java
+++ modules/apps/dispatch/dispatch-test/src/testIntegration/java/com/liferay/dispatch/service/test/DispatchTriggerLocalServiceTest.java
@@ -21,6 +21,7 @@ import com.liferay.dispatch.exception.DuplicateDispatchTriggerException;
 import com.liferay.dispatch.executor.DispatchTaskClusterMode;
 import com.liferay.dispatch.model.DispatchTrigger;
 import com.liferay.dispatch.service.DispatchTriggerLocalService;
+import com.liferay.dispatch.service.test.util.CronExpressionUtil;
 import com.liferay.dispatch.service.test.util.DispatchTriggerTestUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
@@ -191,8 +192,10 @@ public class DispatchTriggerLocalServiceTest {
 					dispatchTrigger.getDispatchTriggerId(),
 					expectedDispatchTrigger.isActive(),
 					expectedDispatchTrigger.getCronExpression(),
-					dispatchTaskClusterMode, 5, 5, 2024, 11, 11, false, true, 4,
-					4, 2024, 12, 0);
+					dispatchTaskClusterMode, CronExpressionUtil.getMonth() + 1,
+					20, CronExpressionUtil.getYear(), 23, 59, false, true,
+					CronExpressionUtil.getMonth() - 1, 1,
+					CronExpressionUtil.getYear(), 0, 0);
 
 			_basicAssertEquals(expectedDispatchTrigger, dispatchTrigger);
 
