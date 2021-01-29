fb32c8ee8bd107eb551db5322ff1a6a98e389113
Brian Chan <brian.chan@liferay.com>
2021-01-29 12:58:48 -0300

    LPS-126159 Sort

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
@@ -94,17 +94,15 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 		Callable<Layout> callable = new CopyLayoutCallable(
 			sourceLayout, targetLayout);
 
-		ServiceContext currentServiceContext =
-			ServiceContextThreadLocal.getServiceContext();
-
 		boolean copyLayout = CopyLayoutThreadLocal.isCopyLayout();
-
 		boolean stagingAdvicesThreadLocalEnabled =
 			StagingAdvicesThreadLocal.isEnabled();
 
+		ServiceContext currentServiceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
 		try {
 			CopyLayoutThreadLocal.setCopyLayout(true);
-
 			StagingAdvicesThreadLocal.setEnabled(false);
 
 			return TransactionInvokerUtil.invoke(_transactionConfig, callable);
@@ -113,12 +111,11 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 			throw new Exception(throwable);
 		}
 		finally {
-			ServiceContextThreadLocal.pushServiceContext(currentServiceContext);
-
+			CopyLayoutThreadLocal.setCopyLayout(copyLayout);
 			StagingAdvicesThreadLocal.setEnabled(
 				stagingAdvicesThreadLocalEnabled);
 
-			CopyLayoutThreadLocal.setCopyLayout(copyLayout);
+			ServiceContextThreadLocal.pushServiceContext(currentServiceContext);
 		}
 	}
 
