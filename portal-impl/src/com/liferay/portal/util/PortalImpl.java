97109177fe85a995f15ed2f7ef5a15a8b953392a
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2021-02-02 08:27:29 +0100

    LPS-126937 Check web.server.forwarded.protocol.enabled to get the corresponding CDN Host (http or https)

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1542,7 +1542,7 @@ public class PortalImpl implements Portal {
 
 		Company company = getCompany(httpServletRequest);
 
-		if (httpServletRequest.isSecure()) {
+		if (isSecure(httpServletRequest)) {
 			cdnHost = getCDNHostHttps(company.getCompanyId());
 		}
 		else {
