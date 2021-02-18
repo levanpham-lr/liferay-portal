80d66d4b6a46328af1ce0c945e9e91ce3733b474
Victor Galan <victor.galan@liferay.com>
2021-02-18 11:39:04 +0100

    LPS-126483 Use new custom color picker API to select custom values

--- modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ColorPickerInput.es.js
+++ modules/apps/portlet-configuration/portlet-configuration-css-web/src/main/resources/META-INF/resources/js/ColorPickerInput.es.js
@@ -17,16 +17,20 @@ import React, {useState} from 'react';
 
 const ColorPicker = ({color, label, name}) => {
 	const [colorValue, setColorValue] = useState(color);
+	const [customColors, setCustomColors] = useState([]);
 
 	return (
 		<div className="form-group">
 			<input name={name} type="hidden" value={colorValue} />
 
 			<ClayColorPicker
+				colors={customColors}
 				label={label}
 				name={`${name}ColorPicker`}
+				onColorsChange={setCustomColors}
 				onValueChange={setColorValue}
 				showHex={true}
+				showPredefinedColorsWithCustom
 				title={label}
 				value={colorValue}
 			/>
