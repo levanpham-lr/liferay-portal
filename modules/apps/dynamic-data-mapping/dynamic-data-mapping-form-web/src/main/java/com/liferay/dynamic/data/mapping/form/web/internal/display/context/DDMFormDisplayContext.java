e33fa9e9d762a40e5d492b67dd3c36a1b783595d
Carolina <carolina.barbosa@liferay.com>
2021-02-24 09:47:42 -0300

    LPS-128291 Rename the setting Maximum Submissions for Guest Upload Fields to Guest Upload Maximum Submissions

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -192,8 +192,7 @@ public class DDMFormDisplayContext {
 		boolean maximumSubmissionLimitReached =
 			DDMFormGuestUploadFieldUtil.isMaximumSubmissionLimitReached(
 				ddmFormInstance, _getHttpServletRequest(),
-				_ddmFormWebConfiguration.
-					maximumSubmissionsForGuestUploadFields());
+				_ddmFormWebConfiguration.guestUploadMaximumSubmissions());
 
 		boolean requireCaptcha = isCaptchaRequired(ddmFormInstance);
 
