952251e03ce8b9d096cc64ab1339ae6d3e2b3b69
ChrisKian <christopher.kian@liferay.com>
2021-03-08 15:04:59 -0800

    LPS-128817 No longer needed since we are filtering in the search methods instead.

--- modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/internal/display/context/SiteBrowserDisplayContext.java
+++ modules/apps/site/site-browser-web/src/main/java/com/liferay/site/browser/web/internal/display/context/SiteBrowserDisplayContext.java
@@ -35,7 +35,6 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
-import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
@@ -221,8 +220,6 @@ public class SiteBrowserDisplayContext {
 				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 				groupSearch.getOrderByComparator());
 
-			groups = _filterGroups(groups, themeDisplay.getPermissionChecker());
-
 			total = groups.size();
 
 			total += additionalSites;
@@ -390,29 +387,6 @@ public class SiteBrowserDisplayContext {
 		}
 
 		return false;
-	}
-
-	private List<Group> _filterGroups(
-			List<Group> groups, PermissionChecker permissionChecker)
-		throws Exception {
-
-		boolean filterManageableGroups = ParamUtil.getBoolean(
-			_httpServletRequest, "filterManageableGroups", true);
-
-		List<Group> filteredGroups = new ArrayList<>();
-
-		for (Group group : groups) {
-			if (permissionChecker.isGroupAdmin(group.getGroupId()) ||
-				(!filterManageableGroups &&
-				 GroupPermissionUtil.contains(
-					 permissionChecker, group.getGroupId(),
-					 ActionKeys.ASSIGN_MEMBERS))) {
-
-				filteredGroups.add(group);
-			}
-		}
-
-		return filteredGroups;
 	}
 
 	private List<Group> _filterGroups(List<Group> groups, String filter) {
