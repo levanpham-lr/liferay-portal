a20531484c949d4f861835201f07d0b9583ca162
balazssk <balazs.safrany-kovalik@liferay.com>
2021-04-07 13:21:20 +0200

    LPS-130136 LayoutFullURL is no longer used in Journal

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7162,15 +7162,6 @@ public class JournalArticleLocalServiceImpl
 
 				serviceContext.setCommand(Constants.UPDATE);
 
-				String portletId = PortletProviderUtil.getPortletId(
-					JournalArticle.class.getName(),
-					PortletProvider.Action.EDIT);
-
-				String layoutFullURL = _portal.getLayoutFullURL(
-					article.getGroupId(), portletId);
-
-				serviceContext.setLayoutFullURL(layoutFullURL);
-
 				serviceContext.setScopeGroupId(article.getGroupId());
 
 				journalArticleLocalService.updateStatus(
