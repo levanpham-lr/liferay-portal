bcb35ad834353f82634a4f5b14844639c672a4f9
Brian Chan <brian.chan@liferay.com>
2021-04-27 23:17:09 -0300

    LPS-129712 sort

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormFieldTemplateContextFactory.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormFieldTemplateContextFactory.java
@@ -125,6 +125,8 @@ public class DDMFormFieldTemplateContextFactory {
 		DDMFormFieldRenderingContext ddmFormFieldRenderingContext =
 			new DDMFormFieldRenderingContext();
 
+		ddmFormFieldRenderingContext.setDDMFormInstanceId(
+			_ddmFormRenderingContext.getDDMFormInstanceId());
 		ddmFormFieldRenderingContext.setHttpServletRequest(
 			_ddmFormRenderingContext.getHttpServletRequest());
 		ddmFormFieldRenderingContext.setHttpServletResponse(
@@ -141,8 +143,6 @@ public class DDMFormFieldTemplateContextFactory {
 			_ddmFormRenderingContext.isReturnFullContext());
 		ddmFormFieldRenderingContext.setViewMode(
 			_ddmFormRenderingContext.isViewMode());
-		ddmFormFieldRenderingContext.setDDMFormInstanceId(
-			_ddmFormRenderingContext.getDDMFormInstanceId());
 
 		return ddmFormFieldRenderingContext;
 	}
