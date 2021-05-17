f874262c32b3eb354a67e4c8de0942e2e783b78a
Brian I. Kim <il.kim@liferay.com>
2021-05-17 15:55:07 -0700

    LPS-132376 Revert "LPS-123299 Use showVersions parameter to show expired content in the search results as well"
    This reverts commit fd146cc8dcde40d1545ba8115a06be183d07fc4d.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1141,6 +1141,7 @@ public class JournalDisplayContext {
 		SearchContext searchContext = new SearchContext();
 
 		searchContext.setAndSearch(false);
+		searchContext.setAttribute("head", Boolean.TRUE);
 		searchContext.setAttribute("latest", Boolean.TRUE);
 
 		LinkedHashMap<String, Object> params =
@@ -1172,8 +1173,6 @@ public class JournalDisplayContext {
 				Field.TITLE, getKeywords()
 			).put(
 				"ddmStructureKey", getDDMStructureKey()
-			).put(
-				"head", !showVersions
 			).put(
 				"params", params
 			).build());
