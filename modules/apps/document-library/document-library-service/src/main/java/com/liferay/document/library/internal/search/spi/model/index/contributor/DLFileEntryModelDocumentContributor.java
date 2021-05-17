8ce0f6e73b145600efe4816643fecb545ab7acb3
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-05-17 11:18:52 +0200

    LPS-130384 Auto SF

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
@@ -268,11 +268,17 @@ public class DLFileEntryModelDocumentContributor
 		return sb.toString();
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFileEntryModelDocumentContributor.class);
+
 	@Reference
 	private DDMStructureManager _ddmStructureManager;
 
 	@Reference
 	private DLFileEntryMetadataLocalService _dlFileEntryMetadataLocalService;
+
+	@Reference
+	private InputStreamSanitizer _inputStreamSanitizer;
 
 	@Reference
 	private Portal _portal;
@@ -285,11 +291,5 @@ public class DLFileEntryModelDocumentContributor
 
 	@Reference
 	private TrashHelper _trashHelper;
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DLFileEntryModelDocumentContributor.class);
-
-	@Reference
-	private InputStreamSanitizer _inputStreamSanitizer;
 
 }
