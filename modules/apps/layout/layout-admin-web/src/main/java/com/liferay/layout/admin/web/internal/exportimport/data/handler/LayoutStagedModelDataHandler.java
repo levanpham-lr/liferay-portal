e4153f803cf82c9550e41852b76833cb6dd52baf
Jonathan McCann <jonathan.mccann@liferay.com>
2021-06-01 08:20:55 -0700

    LPS-133383 When importing an asset display page consider it as public in order to import Guest permissions

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
@@ -955,7 +955,15 @@ public class LayoutStagedModelDataHandler
 		importedLayout = _updateCollectionLayoutTypeSettings(
 			portletDataContext, layout, importedLayout);
 
+		privateLayout = portletDataContext.isPrivateLayout();
+
+		if (layout.isTypeAssetDisplay()) {
+			portletDataContext.setPrivateLayout(false);
+		}
+
 		portletDataContext.importClassedModel(layout, importedLayout);
+
+		portletDataContext.setPrivateLayout(privateLayout);
 	}
 
 	protected void exportLayoutIconImage(
