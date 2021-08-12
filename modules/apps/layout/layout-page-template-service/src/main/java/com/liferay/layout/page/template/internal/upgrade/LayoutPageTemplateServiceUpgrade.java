12b6186dd33a642a50e800ac15ea981bf2ef9d86
balazssk <balazs.safrany-kovalik@liferay.com>
2021-08-12 12:05:27 +0200

    LPS-137014 Copy the old LayoutPrototype permissions for the new LayoutPageTemplateEntries

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/LayoutPageTemplateServiceUpgrade.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/LayoutPageTemplateServiceUpgrade.java
@@ -155,6 +155,11 @@ public class LayoutPageTemplateServiceUpgrade
 			new com.liferay.layout.page.template.internal.upgrade.v3_4_2.
 				UpgradeLayoutPageTemplateStructureRel(
 					_fragmentEntryConfigurationParser));
+
+		registry.register(
+			"3.4.2", "3.4.3",
+			new com.liferay.layout.page.template.internal.upgrade.v3_4_3.
+				ResourcePermissionUpgradeProcess());
 	}
 
 	@Reference
