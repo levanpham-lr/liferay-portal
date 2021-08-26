519b05caa6ce2ad8d6cbeb5823186c3de5cca566
Jorge González <jorge.gonzalez@liferay.com>
2021-08-26 17:31:26 +0200

    LPS-137980 Add test to check that escapeRedirect doesn't return null with encoded URL
    Co-authored-by: Alicia Garcia <alicia.garcia@liferay.com>

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplEscapeRedirectTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplEscapeRedirectTest.java
@@ -209,6 +209,18 @@ public class PortalImplEscapeRedirectTest {
 	}
 
 	@Test
+	public void testEscapeRedirectWithRelativeURL() throws Exception {
+		Assert.assertEquals(
+			"user/test/~/control_panel/manage/-/select/image,url/",
+			_portalImpl.escapeRedirect(
+				"user/test/~/control_panel/manage/-/select/image,url/"));
+		Assert.assertEquals(
+			"user/test/~/control_panel/manage/-/select/image%2Clurl/",
+			_portalImpl.escapeRedirect(
+				"user/test/~/control_panel/manage/-/select/image%2Clurl/"));
+	}
+
+	@Test
 	public void testEscapeRedirectWithSubdomains() throws Exception {
 		String[] redirectURLDomainsAllowed =
 			PropsValues.REDIRECT_URL_DOMAINS_ALLOWED;
