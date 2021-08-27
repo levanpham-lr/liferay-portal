2270203e6aa9ccbfd665cddfb5243a1042a77d6e
Nikki Prudencio <nikki.prudencio@liferay.com>
2021-08-27 11:53:42 -0700

    LPS-132587 Regen

--- modules/apps/archived/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/service/persistence/impl/GadgetPersistenceImpl.java
+++ modules/apps/archived/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/service/persistence/impl/GadgetPersistenceImpl.java
@@ -3882,7 +3882,7 @@ public class GadgetPersistenceImpl
 
 			if ((columnNames == null) || (columnNames.length == 0)) {
 				if (baseModel.isNew()) {
-					return FINDER_ARGS_EMPTY;
+					return new Object[0];
 				}
 
 				return null;
@@ -3908,8 +3908,9 @@ public class GadgetPersistenceImpl
 				}
 
 				if (finderPath.isBaseModelResult() &&
-					(FINDER_CLASS_NAME_LIST_WITHOUT_PAGINATION ==
-						finderPath.getCacheName())) {
+					(GadgetPersistenceImpl.
+						FINDER_CLASS_NAME_LIST_WITHOUT_PAGINATION ==
+							finderPath.getCacheName())) {
 
 					finderPathColumnBitmask |= _ORDER_BY_COLUMNS_BITMASK;
 				}
