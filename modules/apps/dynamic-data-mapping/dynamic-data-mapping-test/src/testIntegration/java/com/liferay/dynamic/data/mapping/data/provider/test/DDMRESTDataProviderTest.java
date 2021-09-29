4d6179ce566ecb01cdc4dabd1c1ead24a9af21e6
Brian Chan <brian.chan@liferay.com>
2021-09-28 21:23:15 -0300

    LPS-138332 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/data/provider/test/DDMRESTDataProviderTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/data/provider/test/DDMRESTDataProviderTest.java
@@ -333,7 +333,6 @@ public class DDMRESTDataProviderTest {
 		Assert.assertEquals(
 			DDMDataProviderResponseStatus.OK,
 			ddmDataProviderResponse.getStatus());
-
 		Assert.assertEquals(
 			ddmDataProviderResponse.toString(), 0,
 			ddmDataProviderResponse.size());
