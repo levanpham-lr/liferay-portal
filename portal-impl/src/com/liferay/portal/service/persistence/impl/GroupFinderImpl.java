d423ab030c2c4e3f0176e89c8beb2b33de67efcd
Jorge Avalos <jorge.avalos@liferay.com>
2021-10-13 15:16:15 -0700

    LPS-137424 Rename

--- portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/GroupFinderImpl.java
@@ -1206,11 +1206,11 @@ public class GroupFinderImpl
 			if (key.equals("actionId")) {
 				Long companyId = CompanyThreadLocal.getCompanyId();
 
-				Role admin = RoleLocalServiceUtil.fetchRole(
+				Role adminRole = RoleLocalServiceUtil.fetchRole(
 					companyId, RoleConstants.ADMINISTRATOR);
-				Role siteAdmin = RoleLocalServiceUtil.fetchRole(
+				Role siteAdminRole = RoleLocalServiceUtil.fetchRole(
 					companyId, RoleConstants.SITE_ADMINISTRATOR);
-				Role siteOwner = RoleLocalServiceUtil.fetchRole(
+				Role siteOwnerRole = RoleLocalServiceUtil.fetchRole(
 					companyId, RoleConstants.SITE_OWNER);
 
 				Long userId = (Long)params.get("userId");
@@ -1220,11 +1220,11 @@ public class GroupFinderImpl
 						Group.class.getName(), (String)entry.getValue());
 
 				queryPos.add(RoleLocalServiceUtil.hasUserRole(
-					userId, admin.getRoleId()));
+					userId, adminRole.getRoleId()));
 				queryPos.add(userId);
 
-				queryPos.add(siteAdmin.getRoleId());
-				queryPos.add(siteOwner.getRoleId());
+				queryPos.add(siteAdminRole.getRoleId());
+				queryPos.add(siteOwnerRole.getRoleId());
 				queryPos.add(resourceAction.getBitwiseValue());
 			}
 			else if (key.equals("active") || key.equals("layout") ||
