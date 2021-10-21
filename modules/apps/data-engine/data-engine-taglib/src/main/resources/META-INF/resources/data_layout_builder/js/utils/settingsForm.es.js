d7928c502bb7c2a244f73d04f3cf52142d5ee2f7
Jorge Avalos <jorge.avalos@liferay.com>
2021-10-21 14:15:25 -0700

    LPS-141253 Set drop down menu visibility to false for dataSourceType
    The drop down menu will be not be visible when custom data providers are used.

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/utils/settingsForm.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/utils/settingsForm.es.js
@@ -78,6 +78,7 @@ export const getFilteredSettingsContext = ({
 							name: generateName(name, updatedField),
 							predefinedValue: '["manual"]',
 							readOnly: true,
+							visible: false,
 						};
 					}
 
