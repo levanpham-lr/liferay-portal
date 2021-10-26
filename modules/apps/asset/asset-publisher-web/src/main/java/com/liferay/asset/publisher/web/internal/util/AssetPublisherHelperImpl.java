1220de97eef4f7d8e3d1693b39a69975e81c54de
balazssk <balazs.safrany-kovalik@liferay.com>
2021-10-26 15:08:37 +0200

    LPS-141570 Set the attribute for AssetPublisherHelper

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -449,6 +449,8 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 
 		assetEntryQuery.setOrderByType2(orderByType2);
 
+		assetEntryQuery.setAttribute("showNonindexable", Boolean.TRUE);
+
 		return assetEntryQuery;
 	}
 
