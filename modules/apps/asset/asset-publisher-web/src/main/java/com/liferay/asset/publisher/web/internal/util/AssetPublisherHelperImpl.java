11e4a7678ed0b7ab9fd4dc1fbd0affc8ad1d2950
balazssk <balazs.safrany-kovalik@liferay.com>
2021-10-27 13:29:49 +0200

    LPS-141570 Disable listable filtering for SQL searches

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -429,6 +429,8 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			assetEntryQuery.setLayout(layout);
 		}
 
+		assetEntryQuery.setListable(null);
+
 		String orderByColumn1 = GetterUtil.getString(
 			portletPreferences.getValue("orderByColumn1", "modifiedDate"));
 
