6113364782e20a0ed22fe467dc6d5d3e7edc8216
Brian Chan <brian.chan@liferay.com>
2021-11-03 22:49:55 -0300

    LPS-141749 Simplify

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/DeletionSystemEventExporter.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/DeletionSystemEventExporter.java
@@ -201,8 +201,12 @@ public class DeletionSystemEventExporter {
 				dynamicQuery));
 		actionableDynamicQuery.setCompanyId(portletDataContext.getCompanyId());
 		actionableDynamicQuery.setPerformActionMethod(
-			(SystemEvent systemEvent) -> exportDeletionSystemEvent(
-				portletDataContext, systemEvent, rootElement, systemEventIds));
+			(SystemEvent systemEvent) -> {
+				exportDeletionSystemEvent(
+					portletDataContext, systemEvent, rootElement);
+
+				systemEventIds.add(systemEvent.getSystemEventId());
+			});
 
 		actionableDynamicQuery.performActions();
 
@@ -211,7 +215,7 @@ public class DeletionSystemEventExporter {
 
 	protected void exportDeletionSystemEvent(
 		PortletDataContext portletDataContext, SystemEvent systemEvent,
-		Element deletionSystemEventsElement, List<Long> systemEventIds) {
+		Element deletionSystemEventsElement) {
 
 		Element deletionSystemEventElement =
 			deletionSystemEventsElement.addElement("deletion-system-event");
@@ -240,8 +244,6 @@ public class DeletionSystemEventExporter {
 			new StagedModelType(
 				systemEvent.getClassNameId(),
 				systemEvent.getReferrerClassNameId()));
-
-		systemEventIds.add(systemEvent.getSystemEventId());
 	}
 
 	private DeletionSystemEventExporter() {
