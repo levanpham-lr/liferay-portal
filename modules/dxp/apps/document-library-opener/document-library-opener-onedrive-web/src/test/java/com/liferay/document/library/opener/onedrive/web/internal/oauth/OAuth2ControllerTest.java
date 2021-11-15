95fa6dbab96934fbdfcf73c6f9e9f385702ca11b
Alan Huang <alan.huang@liferay.com>
2021-11-16 00:33:26 +0800

    LPS-105380 SF, use JSONUtil.toString

--- modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/test/java/com/liferay/document/library/opener/onedrive/web/internal/oauth/OAuth2ControllerTest.java
+++ modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/test/java/com/liferay/document/library/opener/onedrive/web/internal/oauth/OAuth2ControllerTest.java
@@ -199,7 +199,9 @@ public class OAuth2ControllerTest {
 		Assert.assertNull(
 			mockHttpServletRequest.getAttribute(WebKeys.REDIRECT));
 		Assert.assertEquals(
-			String.valueOf(JSONUtil.put("redirectURL", "authorizationURL")),
+			JSONUtil.put(
+				"redirectURL", "authorizationURL"
+			).toString(),
 			mockHttpServletResponse.getContentAsString());
 	}
 
