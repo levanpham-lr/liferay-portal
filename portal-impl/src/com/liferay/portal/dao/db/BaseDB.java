357630b0a3e5190316c4dad8df7aea6b3f6f2dd1
shuyangzhou <shuyang.zhou@liferay.com>
2021-11-17 11:34:44 -0800

    LPS-140390 Create customization point to allow subclass to provide a different style of length limiting.

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -767,6 +767,10 @@ public abstract class BaseDB implements DB {
 
 	protected abstract String[] getTemplate();
 
+	protected String limitColumnLength(String column, int length) {
+		return StringBundler.concat(column, "\\(", length, "\\)");
+	}
+
 	protected String replaceTemplate(String template) {
 		if (Validator.isNull(template)) {
 			return null;
@@ -855,13 +859,13 @@ public abstract class BaseDB implements DB {
 
 		StringBuffer sb = new StringBuffer();
 
-		String replacement = "\\(" + stringIndexMaxLength + "\\)";
-
 		while (matcher.find()) {
 			int length = Integer.valueOf(matcher.group(2));
 
 			if (length > stringIndexMaxLength) {
-				matcher.appendReplacement(sb, matcher.group(1) + replacement);
+				matcher.appendReplacement(
+					sb,
+					limitColumnLength(matcher.group(1), stringIndexMaxLength));
 			}
 			else {
 				matcher.appendReplacement(sb, matcher.group(1));
